b17c0d0363c96820c0df9bc33516ae94602a5e0c
Tealpelican <32714135+Tealpelican@users.noreply.github.com>
2023-07-21 14:48:46 +0100

    fixed tagname error

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function test()
 {
     alert("Website load test");
 
-    var table = document.getElementsByClassName("whiteTracks").getElementsByTagName('tbody')[0];
+    var table = document.getElementsByClassName("whiteTracks")[0].getElementsByTagName('tbody')[0];
 
     var rows = table.getElementsByTagName("tr");
     for (var i = 1; i < rows.length; i++) { // start from index 1 to skip the header row
