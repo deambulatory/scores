2b7167947325ea487dbd43bb4b039e2f9cc2e860
Tealpelican <32714135+Tealpelican@users.noreply.github.com>
2023-07-21 16:15:31 +0100

    Added JS to compare whitetrack races & gen table

--- javascript.js
+++ javascript.js
@@ -1,14 +1,37 @@
-function test()
+function runWhiteTrackComparison()
 {
-    alert("Website load test");
+    var score = [0, 0];
 
     var table = document.getElementsByClassName("whiteTracks")[0].getElementsByTagName('tbody')[0];
 
     var rows = table.getElementsByTagName("tr");
-    for (var i = 0; i < 1; i++) { // start from index 1 to skip the header row
+    for (var i = 0; i < rows.length; i++) { // start from index 1 to skip the header row
         var cells = rows[i].getElementsByTagName("td");
-        alert(cells[1].textContent);
+        var classToAdd = "";
+
+        //player 1 wins -> player 2 wins -> draw
+        if(cells[1].textContent < cells[2].textContent) {
+            score[0] += 1;
+            classToAdd = "player1";
+            cells[1].classList.add("bold");
+        } else if(cells[2].textContent < cells[1].textContent) {
+            score[1] += 1;
+            classToAdd = "player2";
+            cells[2].classList.add("bold");
+        } else {
+            score[0] += 1;
+            score[1] += 1;
+            classToAdd = "draw";
+            cells[1].classList.add("bold");
+            cells[2].classList.add("bold");
+        };
+        cells[3].textContent = score[0]+"-"+score[1];
+
+        cells[1].classList.add(classToAdd);
+        cells[2].classList.add(classToAdd);
+        cells[3].classList.add(classToAdd);
     };
+
 };
 
-test();
+runWhiteTrackComparison();
