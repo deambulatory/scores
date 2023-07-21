60e3d8018dcd48b71af8b884afc76a3960da1424
Tealpelican <32714135+Tealpelican@users.noreply.github.com>
2023-07-21 15:14:05 +0100

    Added replay for shortcut

--- javascript.js
+++ javascript.js
@@ -5,15 +5,9 @@ function test()
     var table = document.getElementsByClassName("whiteTracks")[0].getElementsByTagName('tbody')[0];
 
     var rows = table.getElementsByTagName("tr");
-    for (var i = 1; i < rows.length; i++) { // start from index 1 to skip the header row
-      var cells = rows[i].getElementsByTagName("td");
-      if (cells[2].textContent > cells[3]) {
-        alert("player 1 wins");
-      } else if(cells[2].textContent < cells[3]) {
-        alert("player 2 wins");
-      } else {
-        alert("it's a tie");
-      };
+    for (var i = 0; i < 1; i++) { // start from index 1 to skip the header row
+        var cells = rows[i].getElementsByTagName("td");
+        alert(cells[1].textContent);
     };
 };
 
