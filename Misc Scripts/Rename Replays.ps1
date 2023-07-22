7e79e78ccba7f0c16ffc12a52cff52cc105417f8
Player 1 <player1@abcd123player1.co.uk>
2023-07-22 15:19:09 +0100

    B times and replays

---
+++ Misc Scripts/Rename Replays.ps1
@@ -0,0 +1,17 @@
+﻿$path = "" # this should not be the autosave directory, it should be a copy of files
+$items = get-childitem $path | sort name
+
+foreach($item in $items){
+
+    $inputString = $item.name    
+
+    $pattern = "(?<=_)(.*?)(?=-)"
+
+    $newName = $inputString | Select-String -Pattern $pattern | ForEach-Object { $_.Matches.Value }
+
+    $newName += ".gbx"
+
+    Rename-item $item.FullName -NewName $newname
+
+}
+
