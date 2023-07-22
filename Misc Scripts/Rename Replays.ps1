6b8bc810b63a9a2a7bf24885f493c21e2cec2d52
Player 1 <player1@abcd123player1.co.uk>
2023-07-22 16:10:39 +0100

    replay script code change

--- Misc Scripts/Rename Replays.ps1
+++ Misc Scripts/Rename Replays.ps1
@@ -1,17 +1,13 @@
-﻿$path = "" # this should not be the autosave directory, it should be a copy of files
+﻿$path = "C:\Users\$env:username\desktop\Replays"
+copy-item -path "C:\Users\$env:username\Documents\TrackMania\Tracks\Replays\Autosaves" -Recurse -Destination $path 
 $items = get-childitem $path | sort name
 
-foreach($item in $items){
+foreach ($item in $items) {
 
     $inputString = $item.name    
-
     $pattern = "(?<=_)(.*?)(?=-)"
-
     $newName = $inputString | Select-String -Pattern $pattern | ForEach-Object { $_.Matches.Value }
-
     $newName += ".gbx"
-
     Rename-item $item.FullName -NewName $newname
 
 }
-
