37ad6ff55d4e427771c3c4000d73451dca6941ed
Tealpelican <32714135+Tealpelican@users.noreply.github.com>
2023-07-24 17:24:32 +0100

    Added medals to times

--- javascript.js
+++ javascript.js
@@ -1,74 +1,34 @@
-function runWhiteTrackComparison()
-{
-    var score = [0, 0];
+// Check if the user is on a mobile device
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-    var table = document.getElementsByClassName("whiteTracks")[0].getElementsByTagName('tbody')[0];
+const medals = ['🥇', '🥈', '🥉'];
 
-    var rows = table.getElementsByTagName("tr");
-    for (var i = 0; i < rows.length; i++) { // start from index 1 to skip the header row
-        var cells = rows[i].getElementsByTagName("td");
-        var classToAdd = "";
+// If not on a mobile device, add the click event listener
+if (!isMobile) {
 
-        //player 1 wins -> player 2 wins -> draw
-        if(cells[1].textContent < cells[2].textContent) {
-            score[0] += 1;
-            classToAdd = "player1";
-            cells[1].classList.add("bold");
-        } else if(cells[2].textContent < cells[1].textContent) {
-            score[1] += 1;
-            classToAdd = "player2";
-            cells[2].classList.add("bold");
-        } else {
-            score[0] += 1;
-            score[1] += 1;
-            classToAdd = "draw";
-            cells[1].classList.add("bold");
-            cells[2].classList.add("bold");
-        };
-        cells[3].textContent = score[0]+"-"+score[1];
+    // Get a reference to the downloadCell
+    const downloadCells = document.querySelectorAll(".downloadCell");
 
-        cells[1].classList.add(classToAdd);
-        cells[2].classList.add(classToAdd);
-        cells[3].classList.add(classToAdd);
-    };
+    downloadCells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+            const fileName = cell.getAttribute("data-file");
+            const userConfirmation = window.confirm("Do you want to download the replay for " + cell.textContent + "?");
 
-};
+            if (userConfirmation) { downloadFile(fileName); }
+            else { }
+        });
+    });
 
-function runGreenTrackComparison()
-{
-    var score = [0, 0];
+    function downloadFile(fileName) {
 
-    var table = document.getElementsByClassName("greenTracks")[0].getElementsByTagName('tbody')[0];
+        const fileURL = 'https://github.com/deambulatory/scores/raw/main/Replays/' + fileName;
+        const link = document.createElement('a');
+        link.href = fileURL;
+        link.download = fileName;
+        link.click();
+    }
+}
 
-    var rows = table.getElementsByTagName("tr");
-    for (var i = 0; i < rows.length; i++) { // start from index 1 to skip the header row
-        var cells = rows[i].getElementsByTagName("td");
-        var classToAdd = "";
-
-        //player 1 wins -> player 2 wins -> draw
-        if(cells[1].textContent < cells[2].textContent) {
-            score[0] += 1;
-            classToAdd = "player1";
-            cells[1].classList.add("bold");
-        } else if(cells[2].textContent < cells[1].textContent) {
-            score[1] += 1;
-            classToAdd = "player2";
-            cells[2].classList.add("bold");
-        } else {
-            score[0] += 1;
-            score[1] += 1;
-            classToAdd = "draw";
-            cells[1].classList.add("bold");
-            cells[2].classList.add("bold");
-        };
-        cells[3].textContent = score[0]+"-"+score[1];
-
-        cells[1].classList.add(classToAdd);
-        cells[2].classList.add(classToAdd);
-        cells[3].classList.add(classToAdd);
-    };
-
-};
 $(document).ready(function() {
     $.ajax({
         type: "GET",
@@ -112,11 +72,18 @@ $(document).ready(function() {
             if (data.length == headers.length) {
                 let tr = newTable.insertRow();
 
+                let sortedTimes = getTopThree(data);
+
                 for (var j = 1; j < headers.length; j++) {
                     let td = tr.insertCell();
 
                     if(data[j] !== "") {
+                        if(medals[sortedTimes.indexOf(data[j])]) {
+                            td.appendChild(medals[sortedTimes.indexOf(data[j])]+" ");
+                        };
+
                         td.appendChild(document.createTextNode(data[j]));
+
                     } else {
                         td.appendChild(document.createTextNode("--:--.--"));
                     };
@@ -127,31 +94,21 @@ $(document).ready(function() {
         };
     };
 });
-// Check if the user is on a mobile device
-const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-// If not on a mobile device, add the click event listener
-if (!isMobile) {
+function getTopThree(arr) {
+    let sortedTimes = arr;
 
-    // Get a reference to the downloadCell
-    const downloadCells = document.querySelectorAll(".downloadCell");
+    //remove the track name and track type
+    sortedTimes.splice(0,2);
 
-    downloadCells.forEach((cell) => {
-        cell.addEventListener("click", () => {
-            const fileName = cell.getAttribute("data-file");
-            const userConfirmation = window.confirm("Do you want to download the replay for " + cell.textContent + "?");
+    //remove blanks
+    sortedTimes = sortedTimes.filter(n => n);
 
-            if (userConfirmation) { downloadFile(fileName); }
-            else { }
-        });
-    });
-
-    function downloadFile(fileName) {
-
-        const fileURL = 'https://github.com/deambulatory/scores/raw/main/Replays/' + fileName;
-        const link = document.createElement('a');
-        link.href = fileURL;
-        link.download = fileName;
-        link.click();
-    }
-}
+    //sort the array, remove any duplicates and truncate to top 3
+    var tafixed =   [...sortedTimes].sort()
+        .filter((v,i,self) => self.indexOf(v) === i)
+        .slice(0, 3);
+    
+    return tafixed
+};
+//🥇🥈🥉
