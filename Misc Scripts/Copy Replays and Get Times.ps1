bd8f8305b9c41107e038c8221499721084d4ae10
Player 1 <player1@abcd123player1.co.uk>
2023-07-30 12:04:00 +0100

    final final script changes

--- Misc Scripts/Copy Replays and Get Times.ps1
+++ Misc Scripts/Copy Replays and Get Times.ps1
@@ -1,7 +1,4 @@
-﻿Clear-Host
-
-
-# Copy replays to desktop and rename the files
+﻿# Copy replays to desktop and rename the files
 #############################################
 
 if (test-path C:\Users\$env:username\desktop\Replays) { 
@@ -26,7 +23,6 @@ foreach ($item in $items) {
 
 }
 
-
 ##########################################
 
 $files = Get-ChildItem "C:\Users\$env:username\Desktop\Replays" -ErrorAction SilentlyContinue
@@ -37,8 +33,6 @@ foreach ($file in $files) {
     
     $flag = $false
     $content = get-content $file.FullName
-    if ($file.FullName -notmatch "A08") { continue }
-
     $pattern = 'times best="(\d+)"'
     $matches = [regex]::Matches($content, $pattern)
     $number = $matches[0].Value  | Select-String -Pattern '\d+' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
@@ -53,27 +47,23 @@ foreach ($file in $files) {
 
         if ($firstTwoNumbers -gt 59) { 
      
-            $minutes = [math]::DivRem($firstTwoNumbers, 60, [ref]$null)
-            $seconds = $firstTwoNumbers % 60
-
             $flag = $true
 
-            $formattedMinutes = "0" + "$minutes" + ":"            
-            if ($seconds.ToString().length -eq 1) {
+            $minutes = [math]::DivRem($firstTwoNumbers, 60, [ref]$null)
+            $seconds = $firstTwoNumbers % 60
+            $formattedMinutes = "0" + "$minutes" + ":"      
 
-                $formattedSeconds = "0" + "$seconds" + ":"
-                
-            }
-
+            if ($seconds.ToString().length -eq 1) { $formattedSeconds = "0" + "$seconds" + ":" }
             else { $formattedSeconds = "$seconds" + "." }
 
         }
 
-        $formattedTime = $formattedMinutes + $formattedSeconds + $lastTwoNumbers
-        
+        $formattedTime = $formattedMinutes + $formattedSeconds + $lastTwoNumbers        
     }
 
     if ($numberWithoutLastDigit.ToString().Length -eq 5) {
+
+        $flag = $true
 
         $firstThreeNumbers = $numberWithoutLastDigit.ToString().Substring(0, 3)
         $lastTwoNumbers = $numberWithoutLastDigit.ToString().Substring($numberWithoutLastDigit.ToString().Length - 2)
@@ -81,16 +71,10 @@ foreach ($file in $files) {
         $minutes = [math]::DivRem($firstThreeNumbers, 60, [ref]$null)
         $seconds = $firstThreeNumbers % 60
 
-        $formattedMinutes = "0" + "$minutes" + ":"            
-        if ($seconds.ToString().length -eq 1) {
+        $formattedMinutes = "0" + "$minutes" + ":"     
 
-            $formattedSeconds = "0" + "$seconds" + ":"
-                
-        }
-
+        if ($seconds.ToString().length -eq 1) { $formattedSeconds = "0" + "$seconds" + ":" }
         else { $formattedSeconds = "$seconds" + "." }
-
-        $flag = $true
         
         $formattedTime = $formattedMinutes + $formattedSeconds + $lastTwoNumbers
     }
@@ -102,19 +86,19 @@ foreach ($file in $files) {
 
     else {
             
-        $number1 = "{0:N2}" -f ($numberWithoutLastDigit / 100)
+        $time = "{0:N2}" -f ($numberWithoutLastDigit / 100)
 
-        if ($number1.ToString().Length -eq 4) {       
+        if ($time.ToString().Length -eq 4) {       
   
-            $string = $number1.ToString()
-            $number1 = $string.PadLeft(5, "0")   
+            $string = $time.ToString()
+            $time = $string.PadLeft(5, "0")   
         }
     
-        if ($number1.ToString().Length -eq 5) { $number1 = "00:" + $number1 }
+        if ($time.ToString().Length -eq 5) { $time = "00:" + $time }
     
         $track = $file.name -replace ".gbx", ""
 
-        "$track`t" + "$number1" |  out-file "C:\Users\$env:username\Desktop\times.txt" -Append
+        "$track`t" + "$time" |  out-file "C:\Users\$env:username\Desktop\times.txt" -Append
     }
 
 }
