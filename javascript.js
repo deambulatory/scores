db386a321a299015c16d7b1d0db1336462a83ebc
deambulatory <63451208+deambulatory@users.noreply.github.com>
2023-08-01 11:40:22 +0100

    fix csv time formatting issue, work in progress on JS

--- javascript.js
+++ javascript.js
@@ -75,7 +75,7 @@ $(document).ready(function () {
                         const pattern = /^[A-Za-z]\d{2}$/;
 
                         if (pattern.test(data[j])) {
-                            
+
                             td.classList.add("downloadCell");
                             td.setAttribute("data-file", data[j]);
 
@@ -142,3 +142,92 @@ if (!isMobile) {
         link.click();
     }
 }
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+const fs = require('fs');
+const csvFilePath = 'data.csv';
+
+// Function to convert time format "mm:ss.ms" to milliseconds
+function timeToMilliseconds(time) {
+    const timeRegex = /^(\d{2}):(\d{2})\.(\d{2,3})$/;
+    const match = time.match(timeRegex);
+  
+    if (!match) {
+      throw new Error(`Invalid time format: ${time}. Time should be in the format "mm:ss.ms".`);
+    }
+  
+    const minutes = parseInt(match[1]);
+    const seconds = parseInt(match[2]);
+    const milliseconds = parseInt(match[3]);
+  
+    if (isNaN(minutes) || isNaN(seconds) || isNaN(milliseconds)) {
+      throw new Error(`Invalid time format: ${time}. Unable to parse time values.`);
+    }
+  
+    return minutes * 60000 + seconds * 1000 + milliseconds;
+  }
+  
+// Function to convert milliseconds to time format "mm:ss.ms"
+function millisecondsToTime(milliseconds) {
+    const minutes = Math.floor(milliseconds / 60000);
+    const seconds = Math.floor((milliseconds % 60000) / 1000);
+    const remainingMilliseconds = milliseconds % 1000;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${remainingMilliseconds.toString().padStart(3, '0').slice(0, 2)}`;
+}
+
+// Function to add an array of time values in "mm:ss.ms" format
+function addTimes(timeArray) {
+    if (!Array.isArray(timeArray) || timeArray.length === 0) {
+        throw new Error('Invalid input: timeArray should be a non-empty array of time values.');
+    }
+
+    // Convert time values to milliseconds and sum them
+    const totalMilliseconds = timeArray.reduce((acc, time) => acc + timeToMilliseconds(time), 0);
+
+    // Convert the total milliseconds back to "mm:ss.ms" format
+    return millisecondsToTime(totalMilliseconds);
+}
+
+fs.readFile(csvFilePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+
+    const rows = data.trim().split('\n');
+    const timeValues = [];
+
+
+    for (let i = 1; i < Math.min(rows.length, 16); i++) {
+
+        let row = rows[i].trim();
+
+        // Handle \r at the end of the row
+        if (row.endsWith('\r')) {
+            row = row.slice(0, -1); // Remove the trailing \r
+        }
+
+        const columns = row.split(',');
+        const timeValue = columns[4].trim();
+        console.log(timeValue)
+        timeValues.push(timeValue);
+
+    }
+
+    // Check if there are at least 15 time values
+    if (timeValues.length >= 15) {
+        try {
+            const totalTime = addTimes(timeValues.slice(0, 15)); // Pass only the first 15 values to the addTimes function
+            console.log('Total Time:', totalTime); // Output: Total Time: 01:32.46 (example result)
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    } else {
+        console.error('Not enough time values in the CSV. Expected 15, but found', timeValues.length);
+    }
+});
+
+
+
